f31c16962be0c6a4c0b7e172033187184988dd4c
ltsbryant1125 <bl629@drexel.edu>
2021-03-09 16:37:26 -0500

    Updated css
    Changed generic background color to a background image. Stylized submit button.

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var selected_sign; //Currently selected sign
 function playSound() {}
 
 const treeObj = document.getElementById('tree');
-const turtleObj = document.getElementById('tree');
+const turtleObj = document.getElementById('turtle');
 const henObj = document.getElementById('hen');
 const birdObj = document.getElementById('bird');
 const ringObj = document.getElementById('ring');
@@ -31,9 +31,56 @@ btnModal.forEach(button => {
 });
 
 const closeModalObj = document.getElementById('btn-modal=close');
-closeModalObj.addEventListener('click', function() {
-    modal.hidden = !modal.hidden;
-});
+//closeModalObj.addEventListener('click', function(){
+//    modal.hidden = !modal.hidden;
+//});
+
+const userSubmitObj = document.getElementById('userSubmit')
+
+userSubmitObj.addEventListener('click', function(){
+    console.log('User submit button clicked');
+
+    const userBday = new Date(document.getElementById('bday').value);
+    console.log('userBday is' + userBday);
+
+    let whichMonth = userBday.getUTCMonth();
+    whichMonth++;
+    console.log('whichMonth is: ' + whichMonth);
+
+    const whichDayOfMonth = userBday.getUTCDate();
+    console.log('whichDayOfMonth is: ' + whichDayOfMonth)
+
+    let AstroSign = "";
+
+    if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
+        AstroSign = "drums";
+      } else if ((whichMonth == 11 && whichDayOfMonth >= 22) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
+        AstroSign = "pipes";
+      } else if ((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 21)) {
+        AstroSign = "lord";
+      } else if ((whichMonth == 9 && whichDayOfMonth >= 23) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
+        AstroSign = "lady";
+      } else if ((whichMonth == 8 && whichDayOfMonth >= 23) || (whichMonth == 9 && whichDayOfMonth <= 22)) {
+        AstroSign = "maid";
+      } else if ((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 22)) {
+        AstroSign = "swan";
+      } else if ((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
+        AstroSign = "geese";
+      } else if ((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 21)) {
+        AstroSign = "ring";
+      } else if ((whichMonth == 4 && whichDayOfMonth >= 20) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
+        AstroSign = "bird";
+      } else if ((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 19)) {
+        AstroSign = "hen";
+      } else if ((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
+        AstroSign = "turtle";
+      } else if ((whichMonth == 1 && whichDayOfMonth >= 20) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
+        AstroSign = "tree";
+      }
+
+      //console.log('AstroSign is: ' + AstroSign);
+      //userPicked(AstroSign);
+})
 
 treeObj.addEventListener('click', function(evt) {
     console.log('tree button clicked');
@@ -57,6 +104,10 @@ ringObj.addEventListener('click', function(evt) {
 
 geeseObj.addEventListener('click', function(evt) {
     console.log('geese button clicked');
+});
+
+swanObj.addEventListener('click', function(evt) {
+    console.log('swan button clicked');
 });
 
 maidObj.addEventListener('click', function(evt) {
